d8dc85169068b17bf19658e7b34d45234e3dfe52
rjm <rjm@rjms-iMac.local>
2018-03-13 01:18:43 -0700

    rename class Deck and setup outline to follow

---
+++ lib/deck.rb
@@ -0,0 +1,17 @@
+#! user/bin/env ruby
+
+class Deck
+
+  def initialize
+    # Insert rank instance variable
+    # Insert suite instance variable
+    # Insert card array
+  end
+
+  #Insert method to shuffle the deck
+
+  #Insert method to draw cards from the deck one at a time until the deck is empty
+
+  #Insert method to count cards
+
+end
